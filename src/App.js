f7332c212cebd3755415dcdf2409498f70a00f29
Bằng <133222169+bangwoo4@users.noreply.github.com>
2024-09-17 23:29:23 +0700

    ⭐ Add completed Register page + Router
    Lot of changes to match the design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import React from "react";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import Register from "./Register";
+import logo from "./images/logo.png";
 import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [error, setError] = useState("");
+  const [logged, setlogged] = useState(false);
+
+  // const testLocalStorage = () => {
+  //   localStorage.setItem("123", "456");
+  //   alert(localStorage.getItem("123"));
+  // };
 
   const handleLogin = (e) => {
     e.preventDefault();
-    // Handle login logic here
   };
 
   const handleEmailChange = (e) => {
@@ -18,60 +27,73 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
+  const handleRegister = () => {
+    setIsRegistering(false);
+  };
+
   return (
-    <div className="loginContainer">
-      <div className="loginBox">
-        <h1 className="login-title">Login to your account</h1>
-        <form className="login-form" onSubmit={handleLogin}>
-          {/* Email Field */}
-          <div className="email-field">
-            <label htmlFor="email">Email</label>
-            <input
-              className="loginInput"
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              placeholder="Enter your email"
-              required
-            ></input>
-          </div>
-          {/* Password Field */}
-          <div className="password-field">
-            <div className="password-text">
-              <label htmlFor="pasword">Password</label>
-              <a className="forgot-link" href="/">
-                Forgot?
-              </a>
-            </div>
-            <div className="passwordInput">
-              <input
-                className="loginInput"
-                type="password"
-                id="password"
-                value={password}
-                onChange={handlePasswordChange}
-                placeholder="Enter your password"
-                required
-              ></input>
-            </div>
-          </div>
-          {/* Submit Button*/}
-          <div className="loginButtonContainer">
-            <button className="loginButton" onClick={handleLogin}>
-              Login now
-            </button>
-          </div>
-          {/* Sign Up */}
-          <div className="signUp-field">
-            <p className="signUp-text"> Don't have an account? </p>
-            <a className="signUp-link" href="/">
-              Sign up
-            </a>
-          </div>
-          {/* End */}
-        </form>
+    <div className="Main">
+      <div className="logo">
+        <img src={logo} alt="logo" />
       </div>
+      {isRegistering ? (
+        <Register onRegister={handleRegister} onCancel={handleRegister} />
+      ) : (
+        <div className="loginContainer">
+          <div className="loginBox">
+            <h1 className="login-title">Login to your account</h1>
+            <form className="login-form" onSubmit={handleLogin}>
+              {/* Email Field */}
+              <div className="email-field">
+                <label htmlFor="email">Email</label>
+                <input
+                  className="loginInput"
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  placeholder="Enter your email"
+                  required
+                ></input>
+              </div>
+              {/* Password Field */}
+              <div className="password-field">
+                <div className="password-text">
+                  <label htmlFor="pasword">Password</label>
+                  <a className="forgot-link" href="/">
+                    Forgot?
+                  </a>
+                </div>
+                <div className="passwordInput">
+                  <input
+                    className="loginInput"
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    placeholder="Enter your password"
+                    required
+                  ></input>
+                </div>
+              </div>
+              {/* Submit Button*/}
+              <div className="loginButtonContainer">
+                <button className="loginButton" onClick={handleLogin}>
+                  Login now
+                </button>
+              </div>
+              {/* Sign Up */}
+              <div className="signUp-field">
+                <p className="signUp-text"> Don't have an account? </p>
+                <a className="signUp-link" href="/register">
+                  Sign up
+                </a>
+              </div>
+              {/* End */}
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
