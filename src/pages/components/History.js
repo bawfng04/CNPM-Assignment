f0d43ad9749c1f842d98619a37e5c263e6fcc7b1
Bằng <133222169+bawfng04@users.noreply.github.com>
2024-09-19 23:09:57 +0700

    add props to change activeComponent
    When clicking the button in the sidebar, the activecomponent state will change and render the corresponding component.

---
+++ src/pages/components/History.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function History() {
+  return <div>History div</div>;
+}
+
+export default History;
--- src/pages/components/Sidebar/Sidebar.js
+++ src/pages/components/Sidebar/Sidebar.js
@@ -5,27 +5,47 @@ import history from "../../images/history.png";
 import accounts from "../../images/accounts.png";
 import settings from "../../images/settings.png";
 
-function Sidebar() {
+function Sidebar({ setActiveComponent }) {
   return (
     <aside className="sidebar">
       <img src={BKPrinter} alt="BKPrinter" className="BKP"></img>
       <nav className="navBar">
         <ul className="routes">
           <li>
-            <img src={home} alt="home" className="sidebarImg"></img>
-            Dashboard
+            <button
+              onClick={() => setActiveComponent("home")}
+              className="sidebarButton"
+            >
+              <img src={home} alt="home" className="sidebarImg"></img>
+              Dashboard
+            </button>
           </li>
           <li>
-            <img src={history} alt="history" className="sidebarImg"></img>
-            History
+            <button
+              onClick={() => setActiveComponent("history")}
+              className="sidebarButton"
+            >
+              <img src={history} alt="history" className="sidebarImg"></img>
+              History
+            </button>
           </li>
           <li>
-            <img src={accounts} alt="accounts" className="sidebarImg"></img>
-            Accounts
+            <button
+              onClick={() => setActiveComponent("accounts")}
+              className="sidebarButton"
+            >
+              <img src={accounts} alt="accounts" className="sidebarImg"></img>
+              Accounts
+            </button>
           </li>
           <li>
-            <img src={settings} alt="settings" className="sidebarImg"></img>
-            Setting
+            <button
+              onClick={() => setActiveComponent("settings")}
+              className="sidebarButton"
+            >
+              <img src={settings} alt="settings" className="sidebarImg"></img>
+              Settings
+            </button>
           </li>
         </ul>
       </nav>
