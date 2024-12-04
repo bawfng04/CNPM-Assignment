52ef3e0065c3e055478a86ac4842d3c4c2d18761
bawfng04 <nguyendinhbang53az@gmail.com>
2024-12-04 14:23:37 +0700

    fix logout

--- src/pages/components/Cards/SettingsComponent/Security.js
+++ src/pages/components/Cards/SettingsComponent/Security.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-const logoutAPI = "http://localhost:4000/logout";
+const logoutAPI = "http://localhost:4000/logout/";
 
 const handleLogout = async () => {
   const response = await fetch(logoutAPI, {
     method: "GET",
-    credentials: "include",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${localStorage.getItem("token")}`,
--- src/pages/components/Sidebar/Unauthorized.js
+++ src/pages/components/Sidebar/Unauthorized.js
@@ -1,4 +1,8 @@
 const Unauthorized = () => {
+  const handleRedirect = () => {
+    window.location.href = "/login";
+  };
+
   return (
     <div
       style={{
@@ -31,6 +35,22 @@ const Unauthorized = () => {
       >
         You are not authorized to view this page
       </p>
+      <button
+        onClick={handleRedirect}
+        style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          fontSize: "1em",
+          cursor: "pointer",
+          backgroundColor: "#007bff",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          transition: "all 0.3 ease",
+        }}
+      >
+        Go to Login
+      </button>
     </div>
   );
 };
